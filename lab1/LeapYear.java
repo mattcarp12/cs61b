6797f02db1ddc316305adf21dc14dc4bf129e4b6
Matt Carpenter <mattcarp88@gmail.com>
2019-05-05 11:47:46 -0400

    update 20190505

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Matt Carpenter
  */
 public class LeapYear {
 
@@ -13,6 +13,17 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+
+private static boolean isLeapYear(int year) {
+    /** Determine if leap year or not */
+    if (year % 400 == 0) { 
+	return true; 
+    } else if (year % 4 == 0 && year % 100 != 0) {
+	return true;
+    } else {
+	return false;
+    }
+}
 
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
